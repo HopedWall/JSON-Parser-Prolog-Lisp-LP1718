b57012ee3fbde778bc68a3739585652d821fe57f
HopedWall <francesco.porto97@gmail.com>
2017-10-29 19:46:35 +0100

    Add files via upload
    Prova

--- json-parsing.pl
+++ json-parsing.pl
@@ -6,6 +6,10 @@ json_parse(JSONString, Object).
 
 json_get(JSON_obj, Fields, Result).
 
+json_load(FileName, JSON).
+
+json_write(JSON, FileName).
 
 
-%%%%  end of file -- json-parsing.pl
+
+%%%%  end of file -- json-parsing.pl prova
