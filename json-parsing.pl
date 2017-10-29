aa66ea1d037cddf98d0d106a7f2d6ab06c4c81b3
HopedWall <francesco.porto97@gmail.com>
2017-10-29 21:08:23 +0100

    Add files via upload

--- json-parsing.pl
+++ json-parsing.pl
@@ -2,11 +2,27 @@
 
 %%%%  json-parsing.pl
 
-json_parse(JSONString, Object).
+
+%%%% json_parse(JSONString, Object)
+
+%Se è un object...
+json_parse(JSONString, Object) :-
+
+% Se è un array...
+json_parse([], []).    
+json_parse([X | Xs], Object) :-
+.
+
+%%%% json_get(JSON_obj, Fields, Result)
 
 json_get(JSON_obj, Fields, Result).
 
+
+%%%% json_load(FileName, JSON).
+
 json_load(FileName, JSON).
+
+%%%% json_write(JSON, Filename).
 
 json_write(JSON, FileName).
 
