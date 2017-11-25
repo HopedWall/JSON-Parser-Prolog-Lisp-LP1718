c0f7fac301fb5d3d4354fcc2a43bf2e43aa5bd88
HopedWall <francesco.porto97@gmail.com>
2017-11-25 12:09:22 +0100

    Add files via upload
    json_parsing --> fatto a metà, manca array e gestione errori
    json_get --> fatto a metà, manca array e gestione liste
    read/write su file --> fatto al 100%
    N.B. = mancano commenti, cercherò di aggiustare

--- json-parsing.pl
+++ json-parsing.pl
@@ -2,40 +2,81 @@
 
 %%%%  json-parsing.pl
 
+
+json([X | Xs]) --> json_array([X | Xs]), !.
 json(Val) --> json_object(Val).
-json([X | Xs]) --> json_array([X | Xs]).
 
-json_object(Val) --> "{", "}".
-json_object(Val) -->  "{", json_member(V), "}".
+json_object([]) --> "{", "}", !.
+json_object([X | Xs]) -->  "{", json_member([X | Xs]), "}".
 
-json_member(Val) --> json_pair(Val).
-json_member(Val) --> json_pair(V1), ",", json_member(V2).
+json_member([]) --> [], !.
+json_member([X | Xs]) --> json_pair(X), json_member(Xs).
 
-json_pair(Val) --> json_string(V1), ":", json_pair(V2).
+json_pair(X,Y) --> {atom_codes(X,Val)}, atomic(Val), ":", json_pair(Y).
 
-json_array(Val) --> "[", "]".
-json_array(Val) --> "[", json_elements(Val), "]".
+json_array([]) --> [], !.
+json_array([X | Xs]) --> json_elements([X | Xs]).
 
-json_elements(Val) --> json_value(Val).
-json_elements(Val) --> json_value(V1), "," , json_elements(V2).
+json_elements([X | Xs]) --> json_value(X), json_elements(Xs).
 
-%json_value(Val) --> json(Val).
-json_value(Val) --> number(Val).
-json_value(Val) --> json_string(Val).
+json_value(Val) --> json(Val).
+json_value(Val) --> {atom_codes(X,Val), term_string(Y,X)}, integer(Y).
+json_value(Val) --> {atom_codes(X,Val)}, string(X).
 
-number(Val) --> digit(Val).
-number(Val) --> digit(V1), ".", digit(V2).
+s --> "" ; (" ";"\t";"\n";"\r"), s.
 
-json_string(Val) --> { string_codes(Val, X), string(X) }.
 
 %%%% json_parse(JSONString, Object)
-json_prova(JSONString, Object) :-
-    atom_codes(JSONString, Codes),
-    phrase(json(X), Codes, _),
-    Object = X.
+%json_parse(JSONString, Object) :-
+%   atom_codes(JSONString, Codes),
+%    phrase(json(X), Codes, _),
+%    Object = X.
+
+json_parse(JSONString, Object) :-
+    %json_array(JSONString, Object),
+    json_obj(JSONString, Object).
+
+json_obj(JSONString, json_obj(Object)) :-
+    term_string(X, JSONString),
+    {Y} = X,
+    json_member(Y, Object).
+
+json_member(JSONString, Object) :-
+    term_to_atom(JSONString, X),
+    split_string(X,",","", L),
+    json_pair(L, Object).
+
+json_pair([], []) :- !.
+json_pair([X | Xs], [(A,B)|Ys] ) :-
+    term_string(Z, X),
+    A:B = Z,
+    checkString(A),
+    checkJNS(B),
+    json_pair(Xs, Ys).
+
+checkString(Val) :- string(Val).
+checkJNS(Val) :- number(Val).
+checkJNS(Val) :- string(Val).
+checkJNS(Val) :- json_parse(Val,_).
+
+
+
 
 %%%% json_get(JSON_obj, Fields, Result)
-%json_get(JSON_obj, Fields, Result).
+json_get(JSON_obj, Fields, Result) :-
+    json_obj(Y) = JSON_obj,
+    json_get_member(Y, Fields, Result).
+
+json_get_member([(X,Y)| _], Z, Result) :-
+    X = Z,
+    !,
+    Result = Y.
+
+json_get_member([_| Xs], Z, Result) :-
+    json_get_member(Xs, Z, Result).
+
+json_get_member([], _, _) :- fail.
+
 
 
 %%%% json_load(FileName, JSON).
@@ -49,7 +90,8 @@ json_load(Filename, JSON) :-
 %%%% json_write(JSON, Filename).
 json_write(JSON, Filename) :-
     open(Filename, write, Out),
-    write(Out, JSON),
+    atom_string(JSON, JSONmod),
+    write(Out, JSONmod),
     close(Out).
 
 %%%%  end of file -- json-parsing.pl
