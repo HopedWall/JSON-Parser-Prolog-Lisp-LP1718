d3e149d9ef7abdef13c1ce6aa673847a8b68e5ab
HopedWall <francesco.porto97@gmail.com>
2017-11-27 14:56:46 +0100

    Quasi finito!
    Manca solo il predicato "tokenizzatore" (e qualche commento...)

--- json-parsing.pl
+++ json-parsing.pl
@@ -8,20 +8,33 @@ json_parse(JSONString, Object) :-
 json_parse(JSONString, Object) :-
     json_obj(JSONString, Object),
     !.
+
+json_obj(JSONString, json_obj()) :-
+    starts_with(JSONString, '{'),
+    ends_with(JSONString, '}'),
+    term_string(X, JSONString),
+    X = {},
+    !.
 json_obj(JSONString, json_obj(Object)) :-
     starts_with(JSONString, '{'),
     ends_with(JSONString, '}'),
-    !,
     term_string(X, JSONString),
     {Y} = X,
+    !,
     json_member(Y, Object).
 
+json_array(JSONString, json_array()) :-
+    starts_with(JSONString, '['),
+    ends_with(JSONString, ']'),
+    term_string(X, JSONString),
+    X = [],
+    !.
 json_array(JSONString, json_array(Object)) :-
     starts_with(JSONString,'['),
     ends_with(JSONString,']'),
-    !,
     term_string(X, JSONString),
     [Y | Ys] = X,
+    !,
     json_values([Y | Ys], Object).
 
 json_values([], []) :- !.
@@ -46,7 +59,7 @@ json_pair([X | Xs], [(A1,B1)|Ys] ) :-
 checkString(Val, Ris) :- string(Val), !, Ris = Val.
 checkJNS(Val,Ris) :- number(Val), !, Ris = Val.
 checkJNS(Val,Ris) :- string(Val), !, Ris = Val.
-%checkJNS(Val,Ris) :- json_parse(Val,Ris).
+checkJNS(Val,Ris) :- json_parse(Val,Ris).
 
 starts_with(String, Char) :-
     string_chars(String, [X|_]),
@@ -65,12 +78,25 @@ check_ends_with([_|Xs],Char) :-
 
 
 %%%% json_get(JSON_obj, Fields, Result)
-json_get(JSON_obj, Fields, Result) :-
+json_get(_, [], _) :- !, fail.
+json_get(JSON_obj, [X], Result) :-
+    json_get_elements(JSON_obj, X, Result),
+    !.
+json_get(JSON_obj, [X|Xs], Result) :-
+    json_get_elements(JSON_obj, X, Temp),
+    !,
+    json_get(Temp, Xs, Result).
+json_get(JSON_obj, X, Result) :-
+    json_get_elements(JSON_obj, X, Result).
+
+json_get_elements(JSON_obj, Fields, Result) :-
     json_obj(Y) = JSON_obj,
+    !,
     json_get_member(Y, Fields, Result).
-json_get(JSON_obj, Fields , Result) :-
+json_get_elements(JSON_obj, Index , Result) :-
     json_array([X|Xs]) = JSON_obj,
-    json_get_member_position([X | Xs], Fields, Result).
+    !,
+    json_get_member_position([X | Xs], Index, Result).
 
 json_get_member([], _, _) :- fail.
 json_get_member([(X,Y)| _], Z, Result) :-
@@ -81,14 +107,13 @@ json_get_member([_| Xs], Z, Result) :-
     json_get_member(Xs, Z, Result).
 
 json_get_member_position([],[_], _) :- fail.
-json_get_member_position([X | _], [Y], Result) :-
+json_get_member_position([X | _], Y, Result) :-
     Y = 0,
     !,
     Result = X.
-json_get_member_position([_ | Xs], [Y], Result) :-
+json_get_member_position([_ | Xs], Y, Result) :-
     Z is Y-1,
-    json_get_member_position(Xs, [Z], Result).
-
+    json_get_member_position(Xs, Z, Result).
 
 
 %%%% json_load(FileName, JSON).
@@ -107,6 +132,10 @@ json_write(JSON, Filename) :-
     close(Out).
 
 %%%%  end of file -- json-parsing.pl
+
+
+
+
 
 
 
